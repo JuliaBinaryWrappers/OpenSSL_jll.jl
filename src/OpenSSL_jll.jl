36959d7b79de1e5c2663ab055ebc8bcf38736dfc
Elliot Saba <staticfloat@gmail.com>
2019-09-26 14:15:44 -0400

    OpenSSL_jll build 1.1.1+0

---
+++ src/OpenSSL_jll.jl
@@ -0,0 +1,31 @@
+module OpenSSL_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# We put these inter-JLL-package API values here so that they are always defined, even if there
+# is no underlying wrapper held within this JLL package.
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("458c3c95-2e84-50aa-8efc-19380b2a3a95"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "OpenSSL", artifacts_toml) for e in artifacts["OpenSSL"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load OpenSSL; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module OpenSSL_jll
