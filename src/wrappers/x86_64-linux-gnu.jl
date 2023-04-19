a6a14cc4479176b6c302482748ba05f6c726e932
jlbuild <juliabuildbot@gmail.com>
2023-04-19 10:08:13 +0000

    OpenSSL_jll build 3.0.8+0

--- src/wrappers/x86_64-linux-gnu.jl
+++ src/wrappers/x86_64-linux-gnu.jl
@@ -2,20 +2,20 @@
 export libcrypto, libssl, openssl
 
 JLLWrappers.@generate_wrapper_header("OpenSSL")
-JLLWrappers.@declare_library_product(libcrypto, "libcrypto.so.1.1")
-JLLWrappers.@declare_library_product(libssl, "libssl.so.1.1")
+JLLWrappers.@declare_library_product(libcrypto, "libcrypto.so.3")
+JLLWrappers.@declare_library_product(libssl, "libssl.so.3")
 JLLWrappers.@declare_executable_product(openssl)
 function __init__()
     JLLWrappers.@generate_init_header()
     JLLWrappers.@init_library_product(
         libcrypto,
-        "lib/libcrypto.so",
+        "lib64/libcrypto.so",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
     JLLWrappers.@init_library_product(
         libssl,
-        "lib/libssl.so",
+        "lib64/libssl.so",
         RTLD_LAZY | RTLD_DEEPBIND,
     )
 
