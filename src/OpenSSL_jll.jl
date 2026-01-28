cf6aa578ccaa9b2218d7148916240e436120b639
jlbuild <juliabuildbot@gmail.com>
2026-01-28 23:58:49 +0000

    OpenSSL_jll build 3.5.5+0

--- src/OpenSSL_jll.jl
+++ src/OpenSSL_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("OpenSSL")
-JLLWrappers.@generate_main_file("OpenSSL", UUID("458c3c95-2e84-50aa-8efc-19380b2a3a95"))
+JLLWrappers.@generate_main_file("OpenSSL", Base.UUID("458c3c95-2e84-50aa-8efc-19380b2a3a95"))
 end  # module OpenSSL_jll
